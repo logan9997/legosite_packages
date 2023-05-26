4b4210bc2145d66db2cf792b0793ad773a8327e4
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-05-26 15:21:08 +0100

    function to return items without recorded pieces added

--- scripts/scripts/database.py
+++ scripts/scripts/database.py
@@ -30,6 +30,20 @@ class DatabaseManagement():
         self.cursor.execute(sql)
         self.con.commit()
 
+    def get_unrecorded_pieces_for_set(self):
+        sql = '''
+            SELECT I.item_id 
+            FROM "App_item" I, "App_theme" TH 
+            WHERE I.item_id = TH.item_id
+                AND item_type = 'S'
+                AND theme_path like 'Star_Wars%'
+                AND I.item_id NOT IN (
+                    SELECT DISTINCT ON (item_id) item_id
+                    FROM "App_pieceparticipation"
+                )
+        '''
+        return self.SELECT(sql)
+
     def get_pieces(self):
         sql = '''
             SELECT piece_id
